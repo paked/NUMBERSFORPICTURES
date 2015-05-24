f21b64f47d4b4a28012fab6fe42a02c2550759b4
Harrison <harrison@theshoebridges.com>
2015-05-25 09:48:22 +1000

    static/{index,main}: add number view
    Numbers are now displayed in the sidebar.

--- static/main.js
+++ static/main.js
@@ -9,17 +9,26 @@ app.controller('MainCtrl', function($scope) {
 
 app.controller('ImageCtrl', function($scope, $http) {
     $scope.image = {};
+    $scope.numbers = [];
+
     $scope.loadImage = function()  {
         $http.get("/api/images/random").success(function(data) {
             console.log(data);
             $scope.image = data.data;
-            console.log("loaded");
+            console.log("loaded: ", $scope.image.id);
+
+            $http.get("/api/images/" + $scope.image.id + "/numbers")
+                .success(function(d) {
+                    $scope.numbers = d.data;
+                });
         });
     };
 
     $scope.go = function() {
-        $http.post("/api/images/" + $scope.image.id + "/numbers/new?number=" + $scope.number);
-        console.log("posted");
+        $http.post("/api/images/" + $scope.image.id + "/numbers/new?number=" + $scope.number)
+            .success(function(d) {
+                $scope.numbers.push(d.data);
+            });
     };
 
     $scope.loadImage();
