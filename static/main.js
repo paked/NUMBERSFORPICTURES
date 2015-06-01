4df327fe561516e2479ffd6542098f160a3ef2c9
Harrison <harrison@theshoebridges.com>
2015-06-02 08:33:31 +1000

    static/{main,templates/submit}: add submission logic
    You can now submit through the #/submit route!

--- static/main.js
+++ static/main.js
@@ -18,21 +18,38 @@ app.controller('MainCtrl', function($scope) {
     $scope.hello = "Hello, world!";
 });
 
+app.controller('SubmitCtrl', function($scope, $http, $location) {
+    $scope.url = "";
+
+    $scope.submit = function() {
+        if (!$scope.url) {
+            return;
+        }
+
+        $http.post("/api/images/new?url=" + $scope.url)
+            .success(function(d) {
+                console.log(d);
+                $location.path("/");
+            });
+    };
+});
+
 app.controller('ImageCtrl', function($scope, $http) {
     $scope.image = {};
     $scope.numbers = [];
 
     $scope.loadImage = function()  {
-        $http.get("/api/images/random").success(function(data) {
-            console.log(data);
-            $scope.image = data.data;
-            console.log("loaded: ", $scope.image.id);
+        $http.get("/api/images/random")
+            .success(function(data) {
+                console.log(data);
+                $scope.image = data.data;
+                console.log("loaded: ", $scope.image.id);
 
-            $http.get("/api/images/" + $scope.image.id + "/numbers")
+                $http.get("/api/images/" + $scope.image.id + "/numbers")
                 .success(function(d) {
                     $scope.numbers = d.data;
                 });
-        });
+            });
     };
 
     $scope.go = function() {
